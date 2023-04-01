5f68561acc8a5b27e531840893fbb7a7de860f06
Varun Pal Singh <singhvarun928@gmail.com>
2023-04-01 16:35:02 +0530

    step1 completed

---
+++ experiment/simulation/scripts/main.js
@@ -0,0 +1,5 @@
+function step2() {
+  document.querySelector("#step1").style.visibility = "hidden";
+  document.querySelector("#next").style.visibility = "hidden";
+  document.querySelector("#step2").style.visibility = "visible"
+}
